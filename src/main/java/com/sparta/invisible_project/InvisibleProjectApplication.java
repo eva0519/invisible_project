bb65d51abc9377391fafb5b96a85df36a066d4fe
yeonil <ahnyi527@gmail.com>
2022-10-15 00:28:12 +0900

    Initial commit

---
+++ src/main/java/com/sparta/invisible_project/InvisibleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.invisible_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvisibleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvisibleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/invisible_project/InvisibleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.invisible_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvisibleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
